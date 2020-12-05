5a1dd949ce56d37d7d724551db39b00f540f2d36
walter <walter.rezende@bs2.com>
2020-12-05 10:45:55 -0300

    AboutMe app

---
+++ app/src/main/java/com/walterrezende/aboutme/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.walterrezende.aboutme
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.walterrezende.aboutme.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val myName: MyName by lazy { MyName(getString(R.string.name)) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
+        binding.doneButton.setOnClickListener {
+            addNickName(it)
+        }
+    }
+
+    private fun addNickName(view: View) {
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+            doneButton.visibility = View.GONE
+        }
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
---
+++ app/src/main/java/com/walterrezende/aboutme/MyName.kt
@@ -0,0 +1,6 @@
+package com.walterrezende.aboutme
+
+data class MyName(
+    var name: String = "",
+    var nickname: String = ""
+)
